bf984b7023283c42a0c42d4e5b8db33383415eec
Gajduul <gajduul@gmail.com>
2023-01-15 17:41:14 +0100

    Final Version
    Code tidying up
    Build excutable file

--- src/GeneratedPasswordWindow.java
+++ src/GeneratedPasswordWindow.java
@@ -6,11 +6,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GeneratedPasswordWindow extends JFrame implements ActionListener {
-    JButton copygeneratedPasswordButton,generatePasswordAgainButton,closeWindowButton;
+    JButton copygeneratedPasswordButton, generatePasswordAgainButton, closeWindowButton;
     JLabel generatedPasswordLabel;
-    Font mainStringFont = new Font("Arial", Font.BOLD,30);
-    Font secondStringFont = new Font("Arial", Font.PLAIN,20);
-    public GeneratedPasswordWindow(){
+    Font mainStringFont = new Font("Arial", Font.BOLD, 30);
+    Font secondStringFont = new Font("Arial", Font.PLAIN, 20);
+
+    public GeneratedPasswordWindow() {
         setSize(400, 300);
         setTitle("Password Generator");
         setResizable(false);
@@ -19,32 +20,32 @@ public class GeneratedPasswordWindow extends JFrame implements ActionListener {
 
 
         JLabel mainStringLabel = new JLabel("Password Generator");
-        mainStringLabel.setBounds(45,15,500,50);
+        mainStringLabel.setBounds(45, 15, 500, 50);
 
         mainStringLabel.setFont(mainStringFont);
         add(mainStringLabel);
 
         JLabel describeStringLabel = new JLabel("Your password is:");
-        describeStringLabel.setBounds(110,85,500,50);
+        describeStringLabel.setBounds(110, 85, 500, 50);
         describeStringLabel.setFont(secondStringFont);
         add(describeStringLabel);
 
         generatedPasswordLabel = new JLabel(Generator.generatePassword());
-        generatedPasswordLabel.setBounds(60,150,500,50);
+        generatedPasswordLabel.setBounds(60, 150, 500, 50);
         generatedPasswordLabel.setFont(secondStringFont);
         add(generatedPasswordLabel);
 
         copygeneratedPasswordButton = new JButton("Copy Password");
-        copygeneratedPasswordButton.setBounds(200,160,130,30);
+        copygeneratedPasswordButton.setBounds(200, 160, 130, 30);
         add(copygeneratedPasswordButton);
 
         generatePasswordAgainButton = new JButton("Generate again");
-        generatePasswordAgainButton.setBounds(50,220,130,30);
+        generatePasswordAgainButton.setBounds(50, 220, 130, 30);
 
         add(generatePasswordAgainButton);
 
         closeWindowButton = new JButton("Close Window");
-        closeWindowButton.setBounds(200,220,130,30);
+        closeWindowButton.setBounds(200, 220, 130, 30);
         add(closeWindowButton);
 
         closeWindowButton.addActionListener(this);
@@ -56,14 +57,14 @@ public class GeneratedPasswordWindow extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
-        if(source == copygeneratedPasswordButton){
+        if (source == copygeneratedPasswordButton) {
             String labelToCopy = generatedPasswordLabel.getText();
             StringSelection selection = new StringSelection(labelToCopy);
             Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-            clipboard.setContents(selection,null);
+            clipboard.setContents(selection, null);
         } else if (source == generatePasswordAgainButton) {
             generatedPasswordLabel.setText(Generator.generatePassword());
-        } else if (source==closeWindowButton) {
+        } else if (source == closeWindowButton) {
             this.dispose();
 
         }
--- src/PasswordConstructionInfoWindow.java
+++ src/PasswordConstructionInfoWindow.java
@@ -2,42 +2,43 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.text.html.*;
 
 public class PasswordConstructionInfoWindow extends JFrame implements ActionListener {
     JButton closeWindowButton;
-public PasswordConstructionInfoWindow() {
-    Font mainStringFont = new Font("Arial", Font.BOLD,30);
-    Font secondStringFont = new Font("Arial", Font.PLAIN,20);
-    Font constructionFont = new Font("Arial",Font.PLAIN,15);
 
-    setSize(400, 300);
-    setTitle("Password Generator");
-    setResizable(false);
-    setVisible(true);
-    setLayout(null);
+    public PasswordConstructionInfoWindow() {
+        Font mainStringFont = new Font("Arial", Font.BOLD, 30);
+        Font secondStringFont = new Font("Arial", Font.PLAIN, 20);
+        Font constructionFont = new Font("Arial", Font.PLAIN, 15);
+
+        setSize(400, 300);
+        setTitle("Password Generator");
+        setResizable(false);
+        setVisible(true);
+        setLayout(null);
 
 
-    JLabel mainStringLabel = new JLabel("Password Generator");
-    mainStringLabel.setBounds(45,15,500,50);
-    mainStringLabel.setFont(mainStringFont);
-    add(mainStringLabel);
+        JLabel mainStringLabel = new JLabel("Password Generator");
+        mainStringLabel.setBounds(45, 15, 500, 50);
+        mainStringLabel.setFont(mainStringFont);
+        add(mainStringLabel);
 
-    JLabel secondStringLabel = new JLabel("Password Construction:");
-    secondStringLabel.setFont(secondStringFont);
-    secondStringLabel.setBounds(85,65,300,30);
-    add(secondStringLabel);
+        JLabel secondStringLabel = new JLabel("Password Construction:");
+        secondStringLabel.setFont(secondStringFont);
+        secondStringLabel.setBounds(85, 65, 300, 30);
+        add(secondStringLabel);
 
-    JLabel constructionInfoLabel = new JLabel("<html>-10 Letters Long<br> -Upper Case <br/-Lower Case <br>-Numbers <br>-Special Sings</html>");
-    constructionInfoLabel.setFont(constructionFont);
-    constructionInfoLabel.setBounds(130,100,300,90);
-    add(constructionInfoLabel);
+        JLabel constructionInfoLabel = new JLabel("<html>-10 Letters Long<br> -Upper Case <br/-Lower Case <br>-Numbers <br>-Special Sings</html>");
+        constructionInfoLabel.setFont(constructionFont);
+        constructionInfoLabel.setBounds(130, 100, 300, 90);
+        add(constructionInfoLabel);
 
-    closeWindowButton = new JButton("Close Window");
-    closeWindowButton.setBounds(120,220,130,30);
-    add(closeWindowButton);
-    closeWindowButton.addActionListener(this);
-}
+        closeWindowButton = new JButton("Close Window");
+        closeWindowButton.setBounds(120, 220, 130, 30);
+        add(closeWindowButton);
+        closeWindowButton.addActionListener(this);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
